bb98ac71b2e789a49505734b576f7dc5bdf3fccf
Gustavo Luiz <70780066+luiz-lgrp@users.noreply.github.com>
2022-07-23 17:37:30 -0300

    update Linq ListaBrasilMundo

--- ListaBrasilMundo/Program.cs
+++ ListaBrasilMundo/Program.cs
@@ -1,106 +1,76 @@
-﻿List<listaBrasil> LugarPreco;
-LugarPreco = new List<listaBrasil>();
+﻿var listBrazil = new List<Brazil>()
+{
+    new Brazil {Place = "Fernando de Noronha" , Price = 10000},
+    new Brazil {Place = "Rio de Janeiro (capital)" , Price = 2500},
+    new Brazil {Place = "Bonito" , Price = 3500},
+    new Brazil {Place = "Chapada Diamantina" , Price = 8000},
+    new Brazil {Place = "Serras Gaúchas" , Price = 5500},
+    new Brazil {Place = "Salvador" , Price = 4000},
+    new Brazil {Place = "Natal" , Price = 3500},
+    new Brazil {Place = "Amazônia" , Price = 8500},
+    new Brazil {Place = "Búzios (RJ)" , Price = 2800},
+    new Brazil {Place = "Brasília" , Price = 4200}
+};
 
-List<listaMundo> LugarPreco2;
-LugarPreco2 = new List<listaMundo>();
+var listWorld = new List<World>()
+{
+    new World {Place = "Buenos Aires" , Price = 3000},
+    new World {Place = "Nova York" , Price = 15000},
+    new World {Place = "Paris" , Price = 8500},
+    new World {Place = "Tokio" , Price = 12000},
+    new World {Place = "Cairo" , Price = 7000},
+    new World {Place = "Casablanca" , Price = 6800},
+    new World {Place = "Nova Deli" , Price = 8500},
+    new World {Place = "Ilha da Páscoa" , Price = 11000},
+    new World {Place = "Havaí" , Price = 8300},
+    new World {Place = "Qebec" , Price = 7000}
+};
 
-//Lista do Brasil
-LugarPreco.Add(new listaBrasil("Fernando de Noronha",10000));
-LugarPreco.Add(new listaBrasil("Rio de Janeiro (capital)", 2500));
-LugarPreco.Add(new listaBrasil("Bonito", 3500));
-LugarPreco.Add(new listaBrasil("Chapada Diamantina", 8000));
-LugarPreco.Add(new listaBrasil("Serras Gaúchas", 5500));
-LugarPreco.Add(new listaBrasil("Salvador", 4000));
-LugarPreco.Add(new listaBrasil("Natal", 3500));
-LugarPreco.Add(new listaBrasil("Amazônia", 8500));
-LugarPreco.Add(new listaBrasil("Búzios (RJ)", 2800));
-LugarPreco.Add(new listaBrasil("Brasília", 4200));
 
-//Lista do Mundo
-LugarPreco2.Add(new listaMundo("Buenos Aires", 3000));
-LugarPreco2.Add(new listaMundo("Nova York", 15000));
-LugarPreco2.Add(new listaMundo("Paris", 8500));
-LugarPreco2.Add(new listaMundo("Tokio", 12000));
-LugarPreco2.Add(new listaMundo("Cairo", 7000));
-LugarPreco2.Add(new listaMundo("Casablanca", 6800));
-LugarPreco2.Add(new listaMundo("Nova Deli", 8500));
-LugarPreco2.Add(new listaMundo("Ilha da Páscoa", 11000));
-LugarPreco2.Add(new listaMundo("Havaí", 8300));
-LugarPreco2.Add(new listaMundo("Qebec", 7000));
-
-Console.WriteLine("--------------------------------");
-OrdenarMaisCaro();
+Console.WriteLine("\n5 LUGARES MAIS CAROS DO BRASIL");
 Console.WriteLine("");
-Console.WriteLine("--------------------------------");
-OrdenarMaisBarato();
+
+var listB = listBrazil.OrderByDescending(x => x.Price).Take(5);
+
+
+foreach (var result in listB)
+{
+    Console.WriteLine($"Preço: {result.Price} - Lugar: {result.Place}");
+}
+
+
+
 Console.WriteLine("");
-Console.WriteLine("--------------------------------");
+Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+Console.WriteLine("");
 
 
-void OrdenarMaisCaro()
+Console.WriteLine("5 LUGARES MAIS BARATOS DO MUNDO");
+Console.WriteLine("");
+
+var listW = listWorld.OrderBy(x => x.Price).Take(5);
+
+
+foreach (var result in listW)
 {
-  Console.WriteLine("\n5 Lugares mais caros do Brasil");
-  Console.WriteLine("");
-
-  LugarPreco.Sort(delegate (listaBrasil x1, listaBrasil x2)
-  {
-    return x2.Preco.CompareTo(x1.Preco);
-  });
-
-  LugarPreco.RemoveRange(5, 5);
-
-  LugarPreco.ForEach(delegate (listaBrasil x)
-  {
-    Console.WriteLine(String.Format("{0} {1}", x.Preco, x.Lugar));
-  });
+    Console.WriteLine($"Preço: {result.Price} - Lugar: {result.Place}");
 }
 
+Console.ReadKey();
 
 
-void OrdenarMaisBarato()
+
+
+
+class Brazil
 {
-   Console.WriteLine("\n5 Lugares mais baratos do Mundo");
-   Console.WriteLine("");
-
-  LugarPreco2.Sort(delegate (listaMundo x1, listaMundo x2)
-  {
-      return x1.Preco.CompareTo(x2.Preco);
-
-  });
-
-  LugarPreco2.RemoveRange(5, 5);
-
-  LugarPreco2.ForEach(delegate (listaMundo x)
-  {
-      
-      Console.WriteLine(String.Format("{0} {1}", x.Preco, x.Lugar));
-  });
+    public string Place { get; set; }
+    public int Price { get; set; }
 }
 
-public class listaBrasil
+class World
 {
-    //public listaBrasil() { }
-
-    public string Lugar { get; set; }
-    public int Preco { get; set; }
-
-    public listaBrasil(string lugar, int preco)
-    {
-        this.Preco = preco;
-        this.Lugar = lugar;
-    }
+    public string Place { get; set; }
+    public int Price { get; set; }
 }
 
-public class listaMundo
-{
-    //public listaMundo() { }
-
-    public string Lugar { get; set; }
-    public int Preco { get; set; }
-
-    public listaMundo(string lugar, int preco)
-    {
-        this.Preco = preco;
-        this.Lugar = lugar;
-    }
-}
